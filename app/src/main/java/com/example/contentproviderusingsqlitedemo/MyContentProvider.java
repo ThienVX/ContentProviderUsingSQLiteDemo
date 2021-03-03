686604e346d7dc1bb5bcd75fd4d9f87c5b836096
Thien Vu <thienvx@outlook.com>
2021-03-03 11:06:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/contentproviderusingsqlitedemo/MyContentProvider.java
@@ -0,0 +1,148 @@
+package com.example.contentproviderusingsqlitedemo;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+
+public class MyContentProvider extends ContentProvider {
+    public MyContentProvider() {
+    }
+
+    static final String PROVIDER_NAME = "com.thienvx.user.provider";
+
+    // defining content URI
+    static final String URL = "content://" + PROVIDER_NAME + "/users";
+
+    // parsing the content URI
+    static final Uri CONTENT_URI = Uri.parse(URL);
+
+    static final String id = "id";
+    static final String name = "name";
+    static final int uriCode = 1;
+    static final UriMatcher uriMatcher;
+    private static HashMap<String, String> values;
+
+    // creating object of database
+    // to perform query
+    private SQLiteDatabase db;
+
+    // declaring table name of the database
+    static final String TABLE_NAME = "Users";
+
+    static {
+
+        // to match the content URI
+        // every time user access table under content provider
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        // to access whole table
+        uriMatcher.addURI(PROVIDER_NAME, "users", uriCode);
+
+        // to access a particular row
+        // of the table
+        uriMatcher.addURI(PROVIDER_NAME, "users/*", uriCode);
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                return "vnd.android.cursor.dir/users";
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+    }
+    // creating the database
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getWritableDatabase();
+        if (db != null) {
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(TABLE_NAME);
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                qb.setProjectionMap(values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        if (sortOrder == null || sortOrder == "") {
+            sortOrder = id;
+        }
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null,
+                null, "id");
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
+    // adding data to the database
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long rowID = db.insert(TABLE_NAME, "", values);
+        if (rowID > 0) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            return _uri;
+        }
+        throw new SQLiteException("Failed to add a record into " + uri);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    // creating a database
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        // defining a constructor
+        DatabaseHelper(Context context) {
+            super(context, "Users", null, 1);
+        }
+
+        // creating a table in the database
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            db.execSQL(" CREATE TABLE " + TABLE_NAME
+                    + " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + " name TEXT NOT NULL);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+            // sql query to drop a table
+            // having similar name
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+}
